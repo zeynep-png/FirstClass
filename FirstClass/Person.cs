08f103b9a3da84edb48957668075031d8eaabc15
Zeynep Tuana Çakır <2320711004@ogrenci.ibu.edu.tr>
2024-08-02 15:06:16 +0300

    Add project files.

---
+++ FirstClass/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    public class Person         //Person isminde sınıf oluşturuldu
+    {
+
+        public string FirstName { get; set; } //ad soyad doğum tarihi özellikleri
+        public string LastName { get; set; } //soyadın değerini döndür ve set ile değer atama
+        public DateTime BirthDate { get; set; }
+
+        
+        public Person(string firstName, string lastName, DateTime birthDate) // new person oluşum işlemini tek satırda yapmayı amaçladığım için parametreli bir constructer oluşturdum
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+        public Person() { } // new person oluşturma işlemini çok satırda yaparken kullanmak için
+
+        
+        public void PrintInfo() // oluşan personların bilgilerini ekrana yazdıran method
+        {
+            Console.WriteLine($"Ad: {FirstName}, Soyad: {LastName}, Doğum Tarihi: {BirthDate.ToShortDateString()}");
+        }
+
+
+     
+    }
+}
---
+++ FirstClass/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using FirstClass;
+
+Person student1 = new Person(); //yeni öğrenci oluşturma
+student1.FirstName = "Zeynep";
+student1.LastName = "Çakır";
+student1.BirthDate = new DateTime(2001, 11, 6);
+
+Person teacher1 = new Person("Ertan", "Bozkuş", DateTime.Now); // yeni öğretmen oluşturma
+
+student1.PrintInfo(); //method çağırarak bilgileri yazdırma
+teacher1.PrintInfo();
